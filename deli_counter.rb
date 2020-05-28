8d0bb1648e480d9f653a8c13deae87f452eb20be
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-28 18:41:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -36,6 +36,6 @@
     @restaurant = restaurant
     if @restaurant.length == 0
     puts "There is nobody waiting to be served!"
-  else 
-    @restaurant. shift
+  else
+    @restaurant.shift
   end
